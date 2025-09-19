2e0c105ab028cf521daa3df646eb048a1f4c10be
MD Ismail <mmdrismail@gmail.com>
2025-09-19 22:54:49 +0600

    solution3

---
+++ Solution3.java
@@ -0,0 +1,9 @@
+public class Solution3 {
+    private static int numberDayOfWeek = 6; // Sunday - 0, Monday - 1, ..., Saturday - 6.
+
+    public static void main(String[] args) {
+        boolean isWeekEnd = numberDayOfWeek == 0 || numberDayOfWeek == 6;
+
+        System.out.println(isWeekEnd);
+    }
+}
